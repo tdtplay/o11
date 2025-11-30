cd72c06bfdfb9f4621da5c12ad82fa622269feca
tdtplay <36959100+tdtplay@users.noreply.github.com>
2025-11-30 15:42:33 +0000

    process

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 while true; do
   if ! pgrep -f "/home/o11/o11" > /dev/null; then
     echo "Process not found, starting o11..."
-    nohup /home/o11/o11 -p 8283 -noramfs > /home/o11/o11.log 2>&1 &
+    nohup /home/o11/o11 -p 1337 -noramfs > /home/o11/o11.log 2>&1 &
     sleep 10
   else
     echo "Process is running."
